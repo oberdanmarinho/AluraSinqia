617984bc1219c5efff9c0f6d69aec160a58de52b
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-02-02 01:04:48 -0300

    Aula 09

---
+++ Formacao C#/ASP.NET Core parte 1/Web/Catalogo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Web
+{
+    public class Catalogo : ICatalogo
+    {
+        public List<Livro> GetLivros()
+        {
+            var livros = new List<Livro>();
+
+            livros.Add(new Livro("001 ", "Ola mundo 01 ", 100.00m));
+            livros.Add(new Livro("002 ", "Ola mundo 02 ", 140.00m));
+            livros.Add(new Livro("003 ", "Ola mundo 03 ", 150.00m));
+
+            return livros;
+        }
+    }
+}
--- Formacao C#/ASP.NET Core parte 1/Web/Startup.cs
+++ Formacao C#/ASP.NET Core parte 1/Web/Startup.cs
@@ -15,29 +15,32 @@ namespace Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddTransient<ICatalogo, Catalogo>(); // injeção de depenência
+            //services.AddTransient<IRelatorio, Relatorio>(); // injeção de dependência
+
+            //services.AddScoped<ICatalogo, Catalogo>(); // injeção de dependência
+            //services.AddScoped<IRelatorio, Relatorio>(); // injeção de dependência
+
+            var catalogo = new Catalogo();
+
+            services.AddSingleton<ICatalogo>(catalogo);
+            services.AddSingleton<IRelatorio>(new Relatorio(catalogo));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            var livros = new List<Livro>();
-
-            livros.Add(new Livro("001 ", "Ola mundo 01 ", 100.00m));
-            livros.Add(new Livro("002 ", "Ola mundo 02 ", 140.00m));
-            livros.Add(new Livro("003 ", "Ola mundo 03 ", 150.00m));
-
+            ICatalogo catalogo = serviceProvider.GetService<ICatalogo>(); // aqui está sendo usada a injeção de dependência
+            IRelatorio relatorio = serviceProvider.GetService<IRelatorio>(); // aqui está sendo usada a injeção de dependência
+            
             app.Run(async (context) =>
             {
-                foreach (var livro in livros) 
-                
-                {
-                    await context.Response.WriteAsync($"{livro.Codigo,-10}{livro.Nome,-40}{livro.Preco.ToString("C"),10}\r\n");
-                }
+                await relatorio.Imprimir(context);
             });
         }
     }
