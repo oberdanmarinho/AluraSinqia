a3a6b206064c14fd6bc532ed4eda920147f32182
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-03-01 22:30:34 -0300

    Conclusão capitulo 4

---
+++ Formacao C#/(C) C# Parte 1 Primeiros Passos/05.Controle de fluxo com IF/5-CaracteresETextos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o execício 5");
+
+            char primeiraLetra = 'A';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Ola mundo bonito";
+            Console.WriteLine(titulo);
+
+            Console.WriteLine(titulo + primeiraLetra);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Formacao C#/(C) C# Parte 1 Primeiros Passos/05.Controle de fluxo com IF/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 6");
+
+            int idade = 32;
+            int idadeGustvo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustvo);
+
+            Console.ReadLine();
+        }
+    }
+}
