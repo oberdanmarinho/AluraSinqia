264cdaa06c28724f100d331f1442c49b6f010087
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-03-03 23:39:35 -0300

    Modulo 6 - aula 07

---
+++ Formacao C#/(C) C# Parte 1 Primeiros Passos/06.Controle de fluxo com lacos de repeticao/10-CalculaPoupanca/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CalculaPoupanca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Caucula poupança");
+
+            double vlInvest = 1000;
+            int cntMes = 1;
+            string vl;
+
+            while (cntMes <= 12)
+            {
+                vlInvest = vlInvest + vlInvest * 0.0036;
+
+                //if (cntMes <= 1)
+                //{
+                //    Console.WriteLine("Após " + cntMes + " mês, você terá R$" + vlInvest + "!");
+                //}
+                //else 
+                //{
+                //    Console.WriteLine("Após " + cntMes + " meses, você terá R$" + vlInvest + "!");
+                //}
+
+                vl = cntMes <= 1 ? "Após " + cntMes + " mês, você terá R$" + vlInvest + "!" : "Após " + cntMes + " meses, você terá R$" + vlInvest + "!";
+
+                Console.WriteLine(vl);
+
+                cntMes++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Formacao C#/(C) C# Parte 1 Primeiros Passos/06.Controle de fluxo com lacos de repeticao/11-CalculaPoucanca2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_CalculaPoucanca2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 11");
+
+            double vlrInvest = 1000;
+           
+            for (int cntMes = 1; cntMes <= 12; cntMes++)
+            {
+                vlrInvest *= 1.0036;
+                Console.WriteLine("O valor de rendimento é " + vlrInvest + ".");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
