5e16f9c4c0579a5c2f99f24fe20b799256ac35a0
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-03-11 01:18:28 -0300

    C# parte 1: primeiros passos - Conclusão

---
+++ Formacao C#/(C) C# Parte 1 Primeiros Passos/06.Controle de fluxo com lacos de repeticao/15-ProvaFor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_ProvaFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numero;
+
+            for (int multiplo = 1; multiplo <= 100; multiplo++)
+            {
+                //if (multiplo % 3 == 0)
+                //{
+                //    Console.WriteLine("---------> O numero `" + multiplo + "` é múltiplo de 3.");
+                //}
+                //else e
+                //{
+                //    Console.WriteLine("O numero " + multiplo + " náo é múltiplo de 3.");
+                //}
+
+                numero = multiplo % 3 == 0 ? "------>" + multiplo + " é múltiplo de 3." : multiplo + " não é múltiplo de 3.";
+
+                Console.WriteLine(numero);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Formacao C#/(C) C# Parte 1 Primeiros Passos/06.Controle de fluxo com lacos de repeticao/16-Fatorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial = 1;
+
+            for (int n = 1; n <= 10; n++)
+            {
+                fatorial = fatorial * n; // a variável Fatorial sempre vai mudar o seu valor a cada vez que o For passar por ela, e ela vai assumir o valor da ultima passagem
+                Console.WriteLine("O fatorial de " + n + " é " + fatorial);
+            }
+            Console.ReadLine();
+        }
+    }
+}
