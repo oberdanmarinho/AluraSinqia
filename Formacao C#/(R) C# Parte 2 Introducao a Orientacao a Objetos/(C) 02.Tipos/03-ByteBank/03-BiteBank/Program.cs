2a027fd34241293d82aded53c6aeb03f0d7cda44
Oberdan Marinho <oberdanmarinho@gmail.com>
2022-05-18 00:00:54 -0300

    Segunda Etapa - Tipos

---
+++ Formacao C#/(R) C# Parte 2 Introducao a Orientacao a Objetos/(C) 02.Tipos/03-ByteBank/03-BiteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_BiteBank
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      ContaCorrente contaDaGabriela = new ContaCorrente(); // objeto
+      contaDaGabriela.titular = "Gabriela";                 // proprieade - atributo
+      contaDaGabriela.agencia = 863;                        // propriedade - atributo
+      contaDaGabriela.numero = 8652658;                     // propriedade - atributo
+
+      ContaCorrente contaDaGabrielaCosta = new ContaCorrente(); // objeto
+      contaDaGabriela.titular = "Gabriela";                     // propriedade - atributo
+      contaDaGabriela.agencia = 863;                            // propriedade - atributo
+      contaDaGabriela.numero = 8652658;                         // propriedade - atributo
+
+
+      Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+      Console.ReadLine();
+    }
+  }
+}
